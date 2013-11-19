80ddf2620be4fe8b6ee6393397b0e41e5cf17e9c
Hugh Hopkins <hugh.hopkins@gmail.com>
2013-11-19 23:18:01 +0000

    sortOfProgress
    got the .js templates in there without breaking the app but they don't
    do anything

--- client/main.js
+++ client/main.js
@@ -1,1 +1,26 @@
 Meteor.subscribe('posts');
+
+Template.leaderboard.posts = function () {
+  return Posts.find({}, {sort: {score: -1, name: 1}});
+};
+
+Template.postSidebar.selected_name = function () {
+	var post = Posts.findOne(Session.get("selected_postItem"));
+	return post && post.name;
+};
+
+Template.postItem.selected = function () {
+  return Session.equals("selected_postItem", this._id) ? "selected" : '';
+};
+
+Template.postSidebar.events({
+  'click input.inc': function () {
+    Posts.update(Session.get("selected_postItem"), {$inc: {score: 5}});
+  }
+});
+
+Template.post.events ({
+	'click': function () {
+		Session.set("selected_postItem", this._id);
+	}
+});
